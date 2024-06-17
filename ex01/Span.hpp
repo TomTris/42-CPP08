5a8baf6467adaff446650c022818d17613360c65
TomTris <quoctrido.jb@gmail.com>
2024-06-17 19:56:20 +0200

    ex01 with addNumberImproved

---
+++ ex01/Span.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/17 17:55:57 by qdo               #+#    #+#             */
+/*   Updated: 2024/06/17 19:35:17 by qdo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+#include <vector>
+#include <iostream>
+
+class Span {
+private:
+	unsigned int		N;
+	std::vector<int> 	_vec;
+	Span();
+	
+public:
+	~Span();
+	Span(unsigned int const & n);
+	Span(Span const & src);
+	Span & operator=(Span const & src);
+	class MaxN : std::exception{
+		public:
+		const char *what() const throw();	
+	};
+	class TooShort : std::exception{
+		public:
+		const char *what() const throw();	
+	};
+	void	addNumber(int to_add);
+	void	addNumberImproved(int *to_add, int n);
+	int		shortestSpan(void);
+	int		longestSpan(void);
+};
+
+#endif
