baf1d1bad63b26f69119cf04d6dc580458d31b2c
Quoc Tri Do <qdo@1-B-25.42heilbronn.de>
2024-06-18 10:43:39 +0200

    ex02

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 08:22:07 by qdo               #+#    #+#             */
-/*   Updated: 2024/06/18 09:02:14 by qdo              ###   ########.fr       */
+/*   Updated: 2024/06/18 10:24:43 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,56 +19,24 @@
 # include <deque>
 
 template <typename T>
-class MutantStack : std::stack<T, std::vector<T> > {
+class MutantStack : public std::stack<T> {
 
 private:
 
 public:
-	MutantStack();
-	~MutantStack();
-	MutantStack(MutantStack const & src);
-	MutantStack & operator=(MutantStack const & src);
+	~MutantStack() {};
+	MutantStack() {};
+	MutantStack(MutantStack const & src) { *this = src; }
+	MutantStack & operator=(MutantStack const & src) { *this = src;}
 
-	typedef typename std::vector<T>::iterator iterator;
-	typedef typename std::vector<T>::const_iterator const_iterator;
-	typedef typename std::vector<T>::reverse_iterator reverse_iterator;
-	typedef typename std::vector<T>::const_reverse_iterator const_reverse_iterator;
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
-	iterator begin();
-	iterator end();
-	const_iterator begin() const ;
-	const_iterator end() const ;
+	iterator begin() {return this->c.begin();}
+	iterator end() {return this->c.end();}
+
+	const_iterator begin() const {return this->c.begin();};
+	const_iterator end() const {return this->c.begin();};
 };
 
 #endif
-
-template <typename T>
-MutantStack<T>::~MutantStack() {}
-
-template <typename T>
-MutantStack<T>::MutantStack() {}
-
-template <typename T>
-MutantStack<T>::MutantStack(MutantStack const & src)
-{
-	*this = src;
-}
-
-template <typename T>
-MutantStack<T> & MutantStack<T>::operator=(MutantStack const & src)
-{
-	*this = src;
-}
-
-
-template <typename T>
-typename MutantStack<T>::iterator MutantStack<T>::begin() { return this->begin(); }
-
-template <typename T>
-typename MutantStack<T>::iterator MutantStack<T>::end() {return this->end(); }
-
-template <typename T>
-typename MutantStack<T>::const_iterator MutantStack<T>::begin() const {return this->begin(); }
-
-template <typename T>
-typename MutantStack<T>::const_iterator MutantStack<T>::end() const {return this->end(); }
